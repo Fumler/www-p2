50260c1634f87d5a358d8faf60f0335e3adb357c
Fredrik Pettersen <fredrik.fumler@gmail.com>
2013-04-24 01:35:29 +0200

    Login form

--- functions/login.php
+++ functions/login.php
@@ -1,20 +1,11 @@
-<a class="btn btn-primary" data-toggle="modal" href="#myModal">Login</a>
-
-<div class="modal hide" id="myModal">
-    <div class="modal-header">
-        <a class="close" data-dismiss="modal">&times;</a>
-        <h3>Login</h3>
-    </div>
-    <div class="modal-body">
-        <form method="post" action="" id="login_form">
-            <p><input name="uname" type="text" id="uname" value="" placeholder="Username"/></p>
-            <p><input name="pwd" type="password" id="pwd" value="" placeholder="Password"/></p>
+<li class="dropdown">
+    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Log in <strong class="caret"></strong></a>
+    <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+        <form name="login_form" action="index.php" method="post" accept-charset="UTF-8">
+            <legend>Please Log in</legend>
+            <input style="margin-bottom: 15px;" type="text" name="uname" size="30" placeholder="Username" required/ autofocus>
+            <input style="margin-bottom: 15px;" type="password" name="pwd" size="30" placeholder="Password" pattern="(\S{4,10})" required/>
+            <input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; margin-bottom: 15px; font-size: 13px;" type="submit" value="Log in" />
         </form>
     </div>
-    <div class="modal-footer">
-        <a href="javascript:;" onclick="document.getElementById('login_form').submit();" class="btn btn-primary">Login</a>
-        <a href="#" class="btn btn-primary">Register</a>
-        <a href="#" data-dismiss="modal" class="btn">Close</a>
-    </div>
-</div>
-
+</li>
--- functions/signup.php
+++ functions/signup.php
@@ -1,5 +1,5 @@
 <li class="dropdown">
-    <a class="dropdown-toggle" href="#" data-toggle="dropdown"><i class="icon-user"></i> Sign Up <strong class="caret"></strong></a>
+    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign Up <strong class="caret"></strong></a>
     <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
         <form name="registration" action="index.php" method="post" accept-charset="UTF-8">
             <legend>Please sign up</legend>
