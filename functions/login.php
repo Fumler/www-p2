14b0a9c4a542f22eae9575b73bf25137396fea69
Fredrik Pettersen <fredrik.fumler@gmail.com>
2013-04-23 14:40:14 +0200

    Login form

---
+++ functions/login.php
@@ -0,0 +1,22 @@
+<a class="btn btn-primary" data-toggle="modeal" href="#myModal">Login</a>
+
+<div class="modal" id="myModal">
+    <div class="modal-header">
+        <a class="close" data-dismiss="modal">&times;</a>
+        <h3>Login</h3>
+    </div>
+    <div class="modal-body">
+        <form method="post" action="" id="">
+            <input name="uname" type="text" id="uname" value=""/>
+            <input name="pwd" type="password" id="pwd" value=""/>
+
+
+    </div>
+    <div class="modal-footer">
+        <input name="Submit" type="submit" id="submit" value="Login"/>
+        </form>
+        <a href="#" class="btn btn-primary">Register</a>
+        <a href="#" class="btn">Close</a>
+    </div>
+</div>
+
