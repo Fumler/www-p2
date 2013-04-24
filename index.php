b2f9006d7f56c30ec7c26e955fb993b488d23fe2
Inge Dalby <inge.dalby@gmail.com>
2013-04-24 16:26:07 +0200

    Working on #7. Profile viewing works as intended. Editing is next

--- index.php
+++ index.php
@@ -16,6 +16,9 @@ session_start();
     if  (!empty($_GET['logout'])) {
         session_destroy();
     }
+
+    $uid = $user->getID();
+    $profilePage = "pages/profile.php?uid=" . $uid;
 ?>
 
 <!DOCTYPE html>
@@ -100,7 +103,8 @@ session_start();
                     <ul class="nav pull-right">
                         <?php
                         if ($user->loggedOn()) {
-                            echo '<a href="index.php?logout=1">Log out</a>';
+                            echo '<li><a href="#" onclick="ajaxGet(\'' . $profilePage . '\', \'content\')">Profile</a></li>';
+                            echo '<li><a href="index.php?logout=1">Log out</a></li>';
                         } else {
                         ?>
 
