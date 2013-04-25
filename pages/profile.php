1f8c5d8d84868bdbd7eadbdf7daa1651f8233f63
Inge Dalby <inge.dalby@gmail.com>
2013-04-25 21:14:18 +0200

    Continuing profile stuff

--- pages/profile.php
+++ pages/profile.php
@@ -1,10 +1,10 @@
-<div>
-	<legend>Profile</legend>
-</div>
+<h1>Profile</h1>
 
 <?php 
 	
 	require_once('../functions/getUser.php');
+
+	global $user;
 
 	$uid = $_GET['uid'];
 	$userInfo = getUser($uid);
@@ -13,13 +13,15 @@
 
 <form> 
 	<label>Username</label>
-	<input type="text" name="newUname" value="<?php echo $userInfo['uname']; ?>" class="input-xlarge" />
+	<input type="text" name="newUname" value="<?php echo $userInfo['uname']; ?>" class="input-xlarge" disabled />
 	<label>First Name</label>
 	<input type="text" name="newFname" value="<?php echo $userInfo['fname']; ?>" class="input-xlarge" />
 	<label>Last Name</label>
 	<input type="text" name="newLname" value="<?php echo $userInfo['lname']; ?>" class="input-xlarge" />
 	<label>Email</label>
 	<input type="text" name="newEmail" value="<?php echo $userInfo['email']; ?>" class="input-xlarge" />
+	<br /><br />
+	<button class="btn btn-primary">Save changes</button>
 </form>
 
 <form>
@@ -29,4 +31,6 @@
 	<input type="password" name="newPwd" class="input-xlarge" />
 	<label>Repeat New Password</label>
 	<input type="password" name="newPwdR" class="input-xlarge" />
+	<br /><br />
+	<button class="btn btn-primary">Change Password</button>
 </form>
