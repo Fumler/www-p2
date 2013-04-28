da962ad1653314c273b2215c3630c49561b0e902
Christian Holt <ymabob@gmail.com>
2013-04-28 19:27:28 +0200

    Didn't really do much, just some cleaning

--- index.php
+++ index.php
@@ -32,8 +32,12 @@ session_start();
         body { padding-top: 60px; padding-bottom: 40px; }
     </style>
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="css/flat-ui.css" rel="stylesheet">
+    <!-- <link href="css/flat-ui.css" rel="stylesheet"> -->
     <link href="css/custom.css" rel="stylesheet">
+
+    <style type="text/css" id="styles">
+        @import "css/flat-ui.css";
+    </style>
 
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -67,29 +71,9 @@ session_start();
     <script type="text/javascript">
     $(document).ready(function()
     {
-    //  pages.currentPage = -1;
-        console.log("Index.php -> currentPage: " + Pages.currentPage);
+
     });
 
-    /*$("document").ready(function(){
-
-    //     // function loadURL(url) {
-    //     //     console.log("loadURL: " + url);
-    //     //     $("#content").load(url);
-    //     // }
-
-    //     // // $.address.init(function(event) {
-    //     // //     console.log("init: " + $('[rel=address:' + event.value + ']').attr('href'));
-    //     // // }).change(function(event) {
-    //     // //     $("#content").load($('[rel=address:' + event.value + ']').attr('href'));
-    //     // //     console.log("change");
-    //     // // })
-
-    //     // $('a').click(function(){
-    //     //     loadURL($(this).attr('href'));
-    //     // });
-
-        });*/
     </script>
     <div class="navbar navbar-inverse navbar-fixed-top">
         <div class="navbar-inner">
@@ -124,6 +108,7 @@ session_start();
         </div>
 
     </div>
+
     <div id="content" class="span9" >
         <script>
         $(document).ready (function () {
