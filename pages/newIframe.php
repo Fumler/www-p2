6379613c57a21c0bc2a3caa47b14b82230332aee
Inge Dalby <inge.dalby@gmail.com>
2013-04-28 18:18:41 +0200

    iFrame stuff started. up-to-date code is in home.php, will be moved once everything works as intended

---
+++ pages/newIframe.php
@@ -0,0 +1,19 @@
+<div>
+	<a href="#editModal" role="button" class="btn" data-toggle="modal">Edit frame</a>
+	<iframe class="youtube-player" type="text/html" src="http://www.youtube.com/embed/watch?v=4a-apSofamw" width="640" height="385" allowfullscreen frameborder="1" />
+</div>
+
+<!-- The edit modal -->
+<div id="editModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+		<h3 id="editModalLabel">Test modal</h3>
+	</div>
+	<div class="modal-body">
+		<p>Test body</p>
+	</div>
+	<div class="modal-footer">
+		<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+		<button class="btn btn-primary">Save shit</button>
+	</div>
+</div>
