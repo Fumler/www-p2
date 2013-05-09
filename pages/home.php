b46a1749f2e123da8c2db339a13b147ff7e87d13
Inge Dalby <inge.dalby@gmail.com>
2013-05-10 01:57:51 +0200

    home page now displays public pages. bug: clicking home adds duplicates, will fix

--- pages/home.php
+++ pages/home.php
@@ -3,53 +3,66 @@
     $currentPage = -1;
 ?>
 
+
+    <div class="container-fluid">
+        <div class="span9">
+            <legend>Public pages</legend>
+        </div>
+            <div class="span9">
+                <div id="row1" class="row-fluid">
+
+                </div><!--/row-->
+                <div id="row2" class="row-fluid">
+
+                </div><!--/row-->
+                <div id="row3" class="row-fluid">
+
+                </div><!--/row-->
+            </div><!--/span-->
+        </div><!--/row-->
+    </div><!--/.fluid-container-->
+
+
+
+
 <script type="text/javascript">
+    
+    var row = 1;
+    var pub = 1;
 
     $(document).ready(function()
     {
         $("#edit_menu").html('');
         pages.currentPage = -1;
         console.log("home.php -> currentPage: " + pages.currentPage);
+
+        $.ajax ({
+            url: 'functions/fetchPages.php',
+            data: {'privacy': pub},
+            type: 'post',
+            success: function (data) {
+                for (var i = 0; (i < data.length) || (i < 9); i++) {
+                    $("#row" + row).append('<div class="span4">'
+                                                + '<h2>' + data[i].name + '</h2>'
+                                                + '<p><a class="btn" href="javascript:pages.openPage(' 
+                                                + data[i].id
+                                                + ');">View Page &raquo;</a></p>'
+                                            + '</div>');
+
+                    if ((i+1)%3 == 0)
+                        row ++;
+
+                    console.log(i);
+                }
+            }
+        });
+
+        function pageSelected (id)
+        {
+            console.log("Page selected: " + id);
+            pages.currentPage = id;
+        }
+
     });
 
 </script>
-
-    <div class="container-fluid">
-            <div class="span9">
-                <div class="row-fluid">
-                    <div class="span4">
-                        <h2>Test2</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
-                    </div><!--/span-->
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
-                    </div><!--/span-->
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
-                    </div><!--/span-->
-                    </div><!--/row-->
-                    <div class="row-fluid">
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
-                    </div><!--/span-->
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
-                    </div><!--/span-->
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
-                    </div><!--/span-->
-              </div><!--/row-->
-            </div><!--/span-->
-        </div><!--/row-->
-    </div><!--/.fluid-container-->
